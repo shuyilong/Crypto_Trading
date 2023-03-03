334902981d0803a6af0a077b0b91f97ee7ca2519
shuyilong <longshuyi0321@gmail.com>
2023-03-03 12:11:56 -0800

    1

--- Middle_Value/Spot_Snapshot_Second_Data.py
+++ Middle_Value/Spot_Snapshot_Second_Data.py
@@ -10,9 +10,9 @@ from tqdm import tqdm
 def spot_snapshot_second_data():
     Path = path_global.path_spot() + "//binance//book_snapshot_25"
     currency_list = os.listdir(Path)
-    for currency in currency_list[:1]:
+    for currency in currency_list:
         os.chdir(Path + "//" + currency)
-        date_range = [re.findall(r"\d{4}-\d{2}-\d{2}", file)[0] for file in os.listdir()][:16]
+        date_range = [re.findall(r"\d{4}-\d{2}-\d{2}", file)[0] for file in os.listdir()]
         match = re.search(r"\d{4}-\d{2}-\d{2}", os.listdir()[0])
         before, after = os.listdir()[0][:match.start()], os.listdir()[0][match.end():]
         Second_Data = pd.DataFrame()
@@ -24,17 +24,9 @@ def spot_snapshot_second_data():
         for result in tqdm(results, total=len(date_range)):
             Second_Data = pd.concat([Second_Data, result.get()])
         Second_Data.index = range(len(Second_Data))
-        '''
+
         file_path = path_global.path_middle()
         if not os.path.exists(file_path + '//Spot_Snapshot_Second_Data'):
             os.makedirs(file_path + '//Spot_Snapshot_Second_Data')
         os.chdir(file_path + '//Spot_Snapshot_Second_Data')
         Second_Data.to_csv(currency + "_Second_Data.csv")
-        '''
-
-
-
-import time
-start = time.time()
-spot_snapshot_second_data()
-print(time.time() - start)
