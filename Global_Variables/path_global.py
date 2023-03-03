8f4b74dcf3d57546f3e24543b8e7f6a50f18c107
shuyilong <longshuyi0321@gmail.com>
2023-03-02 16:01:27 -0800

    1

---
+++ Global_Variables/path_global.py
@@ -0,0 +1,8 @@
+def path_middle():
+    return "C://Users//Wenjuan Xu//OneDrive//桌面"
+
+def path_spot():
+    return "D://Cryptocurrency Data//Spot"
+
+def path_original():
+    return "D://Cryptocurrency Data"
