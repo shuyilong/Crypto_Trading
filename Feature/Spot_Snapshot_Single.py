afcf74832e5369fdcb85ea018962d0e0c368dc68
shuyilong <longshuyi0321@gmail.com>
2023-03-03 14:29:30 -0800

    1

--- Feature/Spot_Snapshot_Single.py
+++ Feature/Spot_Snapshot_Single.py
@@ -4,12 +4,28 @@ import Data_Clean as DC
 import re
 import pandas as pd
 import time
+import multiprocessing as mp
+from Multi_Processing import Spot_Snapshot_Single_best_bid_diff
+from tqdm import tqdm
 
 def best_bid_diff(symbol, period, begin_date="2022-10-01", end_date= "2023-02-21"):
-    os.chdir(path_global.path_middle() + '//Spot_Snapshot_Second_Data')
+    Path = path_global.path_spot() + "//binance//book_snapshot_25"
+    os.chdir(Path + "//"+symbol)
+    match = re.search(r"\d{4}-\d{2}-\d{2}", os.listdir()[0])
+    before, after = os.listdir()[0][:match.start()], os.listdir()[0][match.end():]
+    date_range = DC.get_date_range(begin_date, end_date)
+    Final_Result = pd.DataFrame()
 
-            ### Get Statistic Data
-            Second_Data = data[['time','asks[0].price']].groupby('time').agg({'asks[0].price': ['mean'], \
-                                'bids[0].price': ['mean'], 'spread':['mean'], 'ask_depth_1':['mean'],\
-                                'ask_depth_5':['mean'], 'ask_depth_15':['mean'], 'bid_depth_1':['mean'],\
-                                'bid_depth_5':['mean'], 'bid_depth_15':['mean']})
+    pool = mp.Pool(processes=mp.cpu_count())
+    results = [pool.apply_async(Spot_Snapshot_Single_best_bid_diff.process_data, args=(date,symbol,period)) \
+               for date in date_range]
+
+    for result in tqdm(results, total=len(date_range)):
+        Final_Result = pd.concat([Final_Result, result.get()])
+    Final_Result.index = range(len(Final_Result))
+
+    file_path = path_global.path_middle() + "//Features"
+    if not os.path.exists(file_path + '//best_bid_diff'):
+        os.makedirs(file_path + '//best_bid_diff')
+    os.chdir(file_path + '//best_bid_diff')
+    Final_Result.to_csv(symbol + ".csv")
---
+++ Multi_Processing/Spot_Snapshot_Single_best_bid_diff.py
@@ -0,0 +1,39 @@
+import os
+import re
+import pandas as pd
+import Data_Clean as DC
+from Global_Variables import path_global
+from functools import lru_cache
+
+begin_date = "2022-10-01"
+end_date = "2023-02-21"
+Path = path_global.path_spot() + "//binance//book_snapshot_25"
+
+
+@lru_cache()
+def process_data(date, symbol, period):
+    os.chdir(Path + "//" + symbol)
+    match = re.search(r"\d{4}-\d{2}-\d{2}", os.listdir()[0])
+    before, after = os.listdir()[0][:match.start()], os.listdir()[0][match.end():]
+
+    time_range = DC.time_interval(date + " 00:00:00", date + " 23:59:59", 1)
+    date_results = []
+
+    if date == begin_date:
+        file_read = [date, DC.Date_Addtion(date, "day", 1)]
+    elif date == end_date:
+        file_read = [DC.Date_Addtion(date, "day", -1), date]
+    else:
+        file_read = [DC.Date_Addtion(date, "day", -1), date, DC.Date_Addtion(date, "day", 1)]
+
+    file = pd.concat([pd.read_csv(before + target + after) for target in file_read])
+    file['time'] = file['timestamp'].apply(lambda x: DC.TimeStamp_to_NormalTime(x))
+
+    for second in time_range:
+        print(second)
+        calculation_time = DC.time_interval(DC.Date_Addtion(second, "second", -period), \
+                                            second, 1)
+        calculation_data = file[file['time'].isin(calculation_time)]
+        date_results.append((calculation_data['bids[0].price'].iloc[-1] - calculation_data['bids[0].price'].iloc[0]) / \
+                            calculation_data['bids[0].price'].iloc[0])
+    return pd.DataFrame({'time': time_range, symbol: date_results})
