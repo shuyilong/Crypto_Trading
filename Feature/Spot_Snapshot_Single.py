59419068e288a85aa872d8f423800701d0c4a9ab
shuyilong <longshuyi0321@gmail.com>
2023-03-03 16:11:29 -0800

    1

--- Feature/Spot_Snapshot_Single.py
+++ Feature/Spot_Snapshot_Single.py
@@ -6,26 +6,35 @@ import pandas as pd
 import time
 import multiprocessing as mp
 from Multi_Processing import Spot_Snapshot_Single_best_bid_diff
-from tqdm.notebook import tqdm_notebook
+from tqdm import tqdm
+import math
 
 def best_bid_diff(symbol, period, begin_date="2022-10-01", end_date= "2023-02-21"):
     Path = path_global.path_spot() + "//binance//book_snapshot_25"
     os.chdir(Path + "//"+symbol)
     match = re.search(r"\d{4}-\d{2}-\d{2}", os.listdir()[0])
     before, after = os.listdir()[0][:match.start()], os.listdir()[0][match.end():]
-    date_range = DC.get_date_range(begin_date, end_date)
-    Final_Result = pd.DataFrame()
+    Date_Range = DC.get_date_range(begin_date, end_date)
 
-    pool = mp.Pool(processes=mp.cpu_count())
-    results = [pool.apply_async(Spot_Snapshot_Single_best_bid_diff.process_data, args=(date, symbol, period)) \
-               for date in date_range]
+    cpu_num = mp.cpu_count()
+    Final_Result_List = []
+    for i in range(math.ceil(len(Date_Range)//cpu_num)):
+        Final_Result = pd.DataFrame()
+        date_range = Date_Range[i*cpu_num : (1+i) * cpu_num]
+        pool = mp.Pool(processes=mp.cpu_count())
+        results = [pool.apply_async(Spot_Snapshot_Single_best_bid_diff.process_data, args=(date, symbol, period)) \
+                   for date in date_range]
+        for result in results:
+            Final_Result = pd.concat([Final_Result, result.get()])
+        Final_Result_List.append(Final_Result)
+        pool.close()
+        pool.join()
 
-    for result in tqdm_notebook(results, desc='Processing data'):
-        Final_Result = pd.concat([Final_Result, result.get()])
-    Final_Result.index = range(len(Final_Result))
-
+    Final_Result_List = pd.concat(Final_Result_List)
+    Final_Result_List.index = range(len(Final_Result_List))
     file_path = path_global.path_middle() + "//Features"
     if not os.path.exists(file_path + '//best_bid_diff'):
         os.makedirs(file_path + '//best_bid_diff')
     os.chdir(file_path + '//best_bid_diff')
-    Final_Result.to_csv(symbol + "_" + str(period) +".csv")
+    Final_Result_List.to_csv(symbol + "_" + str(period) + ".csv")
+
--- Multi_Processing/Spot_Snapshot_Single_best_bid_diff.py
+++ Multi_Processing/Spot_Snapshot_Single_best_bid_diff.py
@@ -4,7 +4,7 @@ import pandas as pd
 import Data_Clean as DC
 from Global_Variables import path_global
 from functools import lru_cache
-from tqdm.notebook import tqdm_notebook
+from tqdm import tqdm
 
 begin_date = "2022-10-01"
 end_date = "2023-02-21"
@@ -17,7 +17,7 @@ def process_data(date, symbol, period):
     match = re.search(r"\d{4}-\d{2}-\d{2}", os.listdir()[0])
     before, after = os.listdir()[0][:match.start()], os.listdir()[0][match.end():]
 
-    time_range = DC.time_interval(date + " 00:00:00", date + " 23:59:59", 1)[:200]
+    time_range = DC.time_interval(date + " 00:00:00", date + " 23:59:59", 1)
     date_results = []
 
     if date == begin_date:
@@ -30,7 +30,7 @@ def process_data(date, symbol, period):
     file = pd.concat([pd.read_csv(before + target + after) for target in file_read])
     file['time'] = file['timestamp'].apply(lambda x: DC.TimeStamp_to_NormalTime(x))
 
-    for second in tqdm_notebook(time_range, desc='Processing data'):
+    for second in tqdm(time_range):
         calculation_time = DC.time_interval(DC.Date_Addtion(second, "second", -period), \
                                             second, 1)
         calculation_data = file[file['time'].isin(calculation_time)]
