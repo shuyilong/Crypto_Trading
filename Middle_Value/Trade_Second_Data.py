8d293564950232593adb3250f61981572c653182
shuyilong <longshuyi0321@gmail.com>
2023-03-04 00:36:32 -0800

    1

--- Middle_Value/Trade_Second_Data.py
+++ Middle_Value/Trade_Second_Data.py
@@ -3,6 +3,9 @@ import re
 import pandas as pd
 import Data_Clean as DC
 from Global_Variables import path_global
+import multiprocessing as mp
+from Multi_Processing import Trade_Second_Data_Use
+from tqdm import tqdm
 
 def trade_second_data():
     ###############################################################################
@@ -13,23 +16,29 @@ def trade_second_data():
     for currency in currency_list:
         print(currency)
         os.chdir(Path + "//" + currency)
-        date_range = [re.findall(r"\d{4}-\d{2}-\d{2}", file)[0] for file in os.listdir()]
+        Date_Range = [re.findall(r"\d{4}-\d{2}-\d{2}", file)[0] for file in os.listdir()]
         match = re.search(r"\d{4}-\d{2}-\d{2}", os.listdir()[0])
         before, after = os.listdir()[0][:match.start()], os.listdir()[0][match.end():]
-        Second_Data = pd.DataFrame()
-        for date in date_range:
-            print(date)
-            target_file = before + date + after
-            data = pd.read_csv(target_file)
-            data['time'] = data['timestamp'].apply(lambda x: DC.TimeStamp_to_NormalTime(x))
-            time_range = DC.time_interval(data['time'].iloc[0], data['time'].iloc[-1], 1)
-            data = data[data['time'].isin(time_range)]
-            data = data.groupby('time').apply(lambda x: x.iloc[-1])
-            Second_Data = pd.concat([Second_Data, data])
 
-        Second_Data.index = range(len(Second_Data))
+        cpu_num = 32
+        Final_Result_List = []
+        for i in range(len(Date_Range) // cpu_num + (len(Date_Range) % cpu_num > 0)):
+            Final_Result = pd.DataFrame()
+            date_range = Date_Range[i * cpu_num: (1 + i) * cpu_num]
+            pool = mp.Pool(processes=cpu_num)
+            results = [pool.apply_async(Trade_Second_Data_Use.process_data, args=(currency, date, \
+                            before, after)) for date in date_range]
+            for result in tqdm(results):
+                Final_Result = pd.concat([Final_Result, result.get()])
+            Final_Result_List.append(Final_Result)
+            pool.close()
+            pool.join()
+
+        Final_Result_List = pd.concat(Final_Result_List)
+        Final_Result_List.index = range(len(Final_Result_List))
         file_path = path_global.path_middle()
         if not os.path.exists(path_global.path_middle() + '//Trade_Second_Data'):
             os.makedirs(path_global.path_middle() + '//Trade_Second_Data')
         os.chdir(path_global.path_middle() + '//Trade_Second_Data')
-        Second_Data.to_csv(currency + "_Second_Data.csv")
+        Final_Result_List.to_csv(currency + "_Second_Data.csv")
+
---
+++ Multi_Processing/Trade_Second_Data_Use.py
@@ -0,0 +1,25 @@
+import os
+import re
+import time
+import numpy as np
+import pandas as pd
+import Data_Clean as DC
+from Global_Variables import path_global
+from functools import lru_cache
+from tqdm import tqdm
+
+Path = path_global.path_spot() + "//binance//trades"
+
+@lru_cache()
+def process_data(currency, date, before, after):
+    os.chdir(Path + "//" + currency)
+    target_file = before + date + after
+    data = pd.read_csv(target_file)
+    data['second_timestamp'] = data['timestamp'] // 1000000
+
+    data['second_timestamp'] = pd.to_datetime(data['second_timestamp'], unit='s')
+    data = data.set_index('second_timestamp')
+    data = data.resample('1S').last()
+    all_seconds = pd.date_range(start=data.index.min(), end=data.index.max(), freq='1s')
+    data = data.reindex(all_seconds).fillna(method="ffill")
+    return data
