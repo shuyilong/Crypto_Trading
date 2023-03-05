e0398afa9454c483edabf1a8a9572e3370ce2f02
shuyilong <longshuyi0321@gmail.com>
2023-03-04 21:08:02 -0800

    1

--- Feature/Spot_Snapshot_Single.py
+++ Feature/Spot_Snapshot_Single.py
@@ -167,6 +167,14 @@ def ask_n_depth(symbol, period, n, data_type, begin_date=path_global.begin_date(
 
 ###################################################################################################
 def window_return(symbol, period, data_type, begin_date=path_global.begin_date(), end_date= path_global.end_date()):
+    ###############################################################################
+    ### This function is for calculating the statistic of return;
+    ### INPUT : 1) symbol, e.g: "BTC"
+    ###         2) period, period of return calculation, in seconds
+    ###         3) data_type, choose from "mean","max","min","std","median","sum"
+    ###         4) begin_date, default in "2022-10-01"
+    ###         5) end, default in "2022-10-01"
+    ###############################################################################
     Path = path_global.path_middle_second_data()
     os.chdir(Path)
     file = pd.read_csv(symbol + "_Second_data.csv")
@@ -192,4 +200,43 @@ def window_return(symbol, period, data_type, begin_date=path_global.begin_date()
         os.makedirs(file_path + '//window_return_'+data_type)
     os.chdir(file_path + '//window_return_'+data_type)
     file.to_csv(symbol + "_" + str(period) + ".csv")
+
+###################################################################################################
+def spread_return(symbol, period, data_type, begin_date=path_global.begin_date(), end_date= path_global.end_date()):
+    ###############################################################################
+    ### This function is for calculating the statistic of spread return;
+    ### INPUT : 1) symbol, e.g: "BTC"
+    ###         2) period, period of return calculation, in seconds
+    ###         3) data_type, choose from "mean","max","min","std","median","sum"
+    ###         4) begin_date, default in "2022-10-01"
+    ###         5) end, default in "2022-10-01"
+    ###############################################################################
+    Path = path_global.path_spot() + "//binance//book_snapshot_25"
+    os.chdir(Path + "//" + symbol)
+    files = sorted(os.listdir())
+    match = re.search(r"\d{4}-\d{2}-\d{2}", files[0])
+    before, after = files[0][:match.start()], files[0][match.end():]
+    Date_Range = DC.get_date_range(begin_date, end_date)
+
+    cpu_num = 32
+    Final_Result_List = []
+    for i in range(len(Date_Range) // cpu_num + (len(Date_Range) % cpu_num > 0)):
+        Final_Result = pd.DataFrame()
+        date_range = Date_Range[i*cpu_num : (1+i) * cpu_num]
+        pool = mp.Pool(processes= cpu_num)
+        results = [pool.apply_async(Spot_Snapshot_Single_.process_data, args=(date, symbol, period, n, data_type)) \
+                   for date in date_range]
+        for result in tqdm(results):
+            Final_Result = pd.concat([Final_Result, result.get()])
+        Final_Result_List.append(Final_Result)
+        pool.close()
+        pool.join()
+
+    Final_Result_List = pd.concat(Final_Result_List)
+    Final_Result_List.index = range(len(Final_Result_List))
+    file_path = path_global.path_middle() + "//Features"
+    if not os.path.exists(file_path + '//spread_return_'+data_type):
+        os.makedirs(file_path + '//spread_return_'+data_type)
+    os.chdir(file_path + '//spread_return_'+data_type)
+    Final_Result_List.to_csv(symbol + "_" + str(period) + ".csv")
 
---
+++ Multi_Processing/Spot_Snapshot_Single_aspread_return.py
@@ -0,0 +1,51 @@
+import os
+import re
+import time
+import numpy as np
+import pandas as pd
+import Data_Clean as DC
+from Global_Variables import path_global
+from functools import lru_cache
+from tqdm import tqdm
+
+begin_date = path_global.begin_date()
+end_date = path_global.end_date()
+Path = path_global.path_spot() + "//binance//book_snapshot_25"
+
+
+@lru_cache()
+def process_data(date, symbol, period, n, data_type):
+    os.chdir(Path + "//" + symbol)
+    match = re.search(r"\d{4}-\d{2}-\d{2}", os.listdir()[0])
+    before, after = os.listdir()[0][:match.start()], os.listdir()[0][match.end():]
+    date_results = []
+
+    if date == begin_date:
+        file_read = [date, DC.Date_Addtion(date, "day", 1)]
+    elif date == end_date:
+        file_read = [DC.Date_Addtion(date, "day", -1), date]
+    else:
+        file_read = [DC.Date_Addtion(date, "day", -1), date, DC.Date_Addtion(date, "day", 1)]
+
+    file = pd.concat([pd.read_csv(before + target + after) for target in file_read])
+    file['second_timestamp'] = file['timestamp'] // 1000000
+    file['second_timestamp'] = pd.to_datetime(file['second_timestamp'], unit='s')
+    file = file.set_index('second_timestamp')
+
+    file['middle_price'] = (file['asks[0].price'] + file['bids[0].price'])/2
+    file['spread_return'] = (file['asks[0].price'] - file['bids[0].price']) / file['middle_price']
+    file = file[['spread_return']].groupby(pd.Grouper(freq='1s')).mean()
+    all_seconds = pd.date_range(start=file.index.min(), end=file.index.max(), freq='1s')
+    file = file.reindex(all_seconds).fillna(method="ffill")
+
+    rolling = file['spread_return'].rolling(window=period, min_periods=1)
+    func_dict = {"mean": rolling.mean, "median": rolling.median, "max": rolling.max, \
+                 "min": rolling.min, "std": rolling.std, "sum": rolling.sum}
+    file['feature'] = func_dict[data_type]().shift(1)
+    file['second_timestamp'] = pd.DatetimeIndex(file.index).astype(int) // 10**9
+
+    start_time = pd.Timestamp(date + ' 00:00:00')
+    end_time = pd.Timestamp(date + ' 23:59:59')
+    mask = file.index.slice_indexer(start_time, end_time)
+    file = file[['second_timestamp','feature']].iloc[mask]
+    return file
