e28e3f83220e615836a982d7a765742b90e6e7d5
shuyilong <longshuyi0321@gmail.com>
2023-03-07 18:13:05 -0800

    1

--- Middle_Value/Monthly_Feature_Calculation.py
+++ Middle_Value/Monthly_Feature_Calculation.py
@@ -1,7 +1,25 @@
-import Load_Data as LD
 import Global_Variables as GV
+import os
+import Feature
+
+###################################################################################################
+def feature_examize(function_list, arg_list):
+    ###############################################################################
+    ### INPUT : 1) function_list, e.g ['best_bid_diff','best_ask_diff']
+    ###         2) arg_list, [("BTC", 300), ("BTC", 300)]
+    ###############################################################################
+    Path = os.path.join(GV.path_middle(), "Features")
+    File_List = []
+    for function, args in zip(function_list, arg_list):
+        args_chain = '_'.join(str(arg) for arg in args)
+        file_path = os.path.join(Path, function, f"{args_chain}.csv")
+        if os.path.exists(file_path):
+            continue
+        else:
+            getattr(Feature, function)(*args)
+            continue
 
 function_map = GV.feature_function_map()
 def monthly_feature_cal():
     for function, args in function_map.items():
-        feature = LD.Load_Feature_Data([function] * len(args), args)
+        feature = feature_examize([function] * len(args), args)
