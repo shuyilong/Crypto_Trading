58c20adfd6ae8de189334da2138252c8a7318724
shuyilong <longshuyi0321@gmail.com>
2023-03-08 17:53:04 -0800

    1

--- Global_Variables/feature_map.py
+++ Global_Variables/feature_map.py
@@ -3,7 +3,8 @@ from itertools import combinations
 
 symbols = ["BTC", "ETH"]
 period = [15, 60, 300, 600]
-order_direction = ["ask", "bid"]
+order_direction1 = ["ask", "bid"]
+order_direction2 = ["ask", "bid", "both"]
 var_direction = ["positive","negative","total"]
 trade_direction = ['buy', 'sell', 'both']
 start = ["2022-10-02"]
@@ -19,8 +20,8 @@ combinations = [(sym, short, long, st, en) for sym, short, long, st, en in \
 
 
 def feature_function_map():
-    Map = {'best_diff' : list(product(symbols, period, order_direction, start, end)),\
-           'n_depth' : list(product(symbols, period, n, data_type1, order_direction, start, end)),\
+    Map = {'best_diff' : list(product(symbols, period, order_direction1, start, end)),\
+           'n_depth' : list(product(symbols, period, n, data_type1, order_direction1, start, end)),\
            'middle_mom' : list(product(symbols, period, start, end)),\
            'spread_return' : list(product(symbols, period, n, data_type2, start, end)),\
            'middle_derivative' : list(product(symbols, period, start, end)),\
@@ -44,13 +45,13 @@ def feature_function_map():
            'slow_sto' : list(product(symbols, period, start, end)),\
            'rsi' : list(product(symbols, period, start, end)),\
            'disparity' : list(product(symbols, period, start, end)),\
-           'order_volumn' : list(product(symbols, period, data_type4, trade_direction, start, end)),\
+           'order_volumn' : list(product(symbols, period, data_type4, order_direction2, start, end)),\
            'order_volumn_imbalance' : list(product(symbols, period, start, end)),\
-           'order_frequency' : list(product(symbols, period, data_type4, trade_direction, start, end)),\
+           'order_frequency' : list(product(symbols, period, data_type4, order_direction2, start, end)),\
            'order_frequency_imbalance' : list(product(symbols, period, start, end)),\
-           'order_volumn_derivative' : list(product(symbols, period, trade_direction, start, end)),\
-           'order_frequency_derivative' : list(product(symbols, period, trade_direction, start, end)),\
-           'order_volumn_derivative_2nd' : list(product(symbols, period, trade_direction, start, end)),\
-           'order_frequency_derivative_2nd' : list(product(symbols, period, trade_direction, start, end)),\
+           'order_volumn_derivative' : list(product(symbols, period, order_direction2, start, end)),\
+           'order_frequency_derivative' : list(product(symbols, period, order_direction2, start, end)),\
+           'order_volumn_derivative_2nd' : list(product(symbols, period, order_direction2, start, end)),\
+           'order_frequency_derivative_2nd' : list(product(symbols, period, order_direction2, start, end)),\
            }
     return Map
